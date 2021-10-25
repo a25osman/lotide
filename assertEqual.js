6aea6a027f56efe4e7ce16ec5b49a9e73e5c6d85
Awab Osman <awabosman3@gmail.com>
2021-10-25 20:42:36 +0000

    assert equal - check if values are equal and return msg

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
