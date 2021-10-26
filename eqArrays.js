b9676a144fe4321f35941a4bfb9bcb693af8d1ed
Awab Osman <awabosman3@gmail.com>
2021-10-26 14:18:15 +0000

    checks if arrays are equal and logs it

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  let check = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      check = false;
+    }
+  }
+
+  if (actual.length !== expected.length) {
+    check = false;
+  }
+
+  if (check === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return check;
+};
+
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
