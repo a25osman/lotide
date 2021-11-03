29198a02a08ca8da36879e9a2727bcbfeef89f7e
Awab Osman <awabosman3@gmail.com>
2021-11-03 03:06:19 -0400

    dry code refactor

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   let foo = array[0];
   return foo;
 };
 
-//assertEqual(head([5,6,7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
